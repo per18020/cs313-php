b2143416b8668b4fff540b22535dd29d2ac645d3
per18020 <per18020@byui.edu>
2020-01-21 15:11:07 -0700

    testing

--- web/week03/team/index.php
+++ web/week03/team/index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Week 03</title>
+
+    <link rel="stylesheet" href="/lib/bulma.min.css">
 </head>
+
 <body>
-    <h1>Week 03</h1>
+    <div class="section">
+        <div class="container">
+            <form action="postForm" method="post">
+                <div class="field">
+                    <label class="label">Name</label>
+                    <div class="control">
+                        <input name="name" id="name" class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
 </body>
+
 </html>
