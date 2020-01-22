cd4bb0d186a98d1ac3855279861f480b26ce6fd2
per18020 <per18020@byui.edu>
2020-01-21 18:32:00 -0700

    Completed team activity for week 03

--- web/week03/team/index.php
+++ web/week03/team/index.php
@@ -1,3 +1,19 @@
+<?php
+
+$continents = [
+    "North America",
+    "South America",
+    "Europe",
+    "Asia",
+    "Australia",
+    "Africa",
+    "Antarctica"
+];
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,18 +24,87 @@
     <title>Week 03</title>
 
     <link rel="stylesheet" href="/lib/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 
 <body>
     <div class="section">
         <div class="container">
+
+            <div class="title">Week 03 Team Assignment</div>
+
             <form action="postForm.php" method="post">
                 <div class="field">
                     <label class="label">Name</label>
-                    <div class="control">
-                        <input name="name" id="name" class="input" type="text" placeholder="Text input">
+                    <div class="control has-icons-left">
+                        <input name="name" class="input" type="text">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
                     </div>
                 </div>
+
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control has-icons-left">
+                        <input name="email" class="input" type="email">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Major</label>
+                    <div class="control">
+                        <label class="radio">
+                            <input type="radio" name="major" value="CS">
+                            Computer Science
+                        </label>
+                        <label class="radio">
+                            <input type="radio" name="major" value="WDD">
+                            Web Design and Development
+                        </label>
+                        <label class="radio">
+                            <input type="radio" name="major" value="CIT">
+                            Computer Information Technology
+                        </label>
+                        <label class="radio">
+                            <input type="radio" name="major" value="CE">
+                            Computer Engineering
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Comments</label>
+                    <div class="control">
+                        <textarea name="comment" class="textarea"></textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Continents You have Visited</label>
+                    <div class="control">
+                        <?php 
+                            foreach ($continents as $continent) {
+                                echo '
+                                    <label class="checkbox">
+                                        <input name="continents[]" type="checkbox" value="' . $continent . '">
+                                        ' . $continent . '
+                                    </label>
+                                ';
+                            }
+                        ?>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button name="submit" class="button is-link">Submit</button>
+                    </div>
+                </div>
+
             </form>
         </div>
     </div>
--- web/week03/team/postForm.php
+++ web/week03/team/postForm.php
@@ -1,3 +1,56 @@
 <?php
 
-    echo $_POST["name"];
+    $name = empty($_POST['name']) ? 'No Name' : filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = empty($_POST['email']) ? 'No Email' : filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $comment = empty($_POST['comment']) ? 'No Comment' : filter_var($_POST["comment"], FILTER_SANITIZE_STRING);
+    $major = empty($_POST['major']) ? 'No Major' : filter_var($_POST["major"], FILTER_SANITIZE_STRING);
+    $continents = $_POST['continents'] ?? ['No Continents Visited'];
+
+    $majorlookup = [
+        "CS" => "Computer Science",
+        "WDD" => "Web Design and Development",
+        "CIT" => "Computer Information Technology",
+        "CE" => "Computer Engineering"
+    ];
+
+    $major = $majorlookup[$major] ?? "No Major";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Week 03 PHP Form Handler</title>
+
+    <link rel="stylesheet" href="/lib/bulma.min.css">
+
+</head>
+<body>
+    <div class="section">
+        <div class="container">
+            <div class="title">Week 03 PHP Form Handler</div>
+            <div class="content">
+                <h2>Name</h2>
+                <p> <?php echo $name ?> </p>
+                <h2>Email</h2>
+                <p> <?php echo $email ?> </p>
+                <h2>Comment</h2>
+                <p> <?php echo $comment ?> </p>
+                <h2>Major</h2>
+                <p> <?php echo $major ?> </p>
+                <h2>Continents Visited</h2>
+                <p> 
+                    <?php 
+                        foreach ($continents as $continent) {
+                            echo $continent;
+                            echo "<br>";
+                        }
+                ?> </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
