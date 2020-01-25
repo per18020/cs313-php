f0dda603fa3f1b87934646b039bb3f840afe59df
per18020 <per18020@byui.edu>
2020-01-24 20:29:24 -0700

    added injection guard with htmlspecialchars()

--- web/week03/assignment/api/savecart.php
+++ web/week03/assignment/api/savecart.php
@@ -2,6 +2,13 @@
 
 session_start();
 
-$_SESSION["cart"] = file_get_contents('php://input');
+$cart = json_decode(file_get_contents('php://input'));
+$cart->info->street = htmlspecialchars($cart->info->street);
+$cart->info->city = htmlspecialchars($cart->info->city);
+$cart->info->state = htmlspecialchars($cart->info->state);
+$cart->info->zip = htmlspecialchars($cart->info->zip);
+$cart->info->country = htmlspecialchars($cart->info->country);
+
+$_SESSION["cart"] = json_encode($cart);
 
 ?>
