7dab7fb1ac86abff9bc269c74c51e1586983b835
per18020 <per18020@byui.edu>
2020-01-30 16:56:07 -0700

    Created db

---
+++ db/project01/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE public.user
+(
+	id serial PRIMARY KEY,
+	email varchar(255) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
+	last_edited_note integer NOT NULL
+);
+
+CREATE TABLE public.note 
+(
+	id serial PRIMARY KEY,
+	user_id integer NOT NULL,
+	folder_id integer,
+	title varchar(255) NOT NULL,
+	creation timestamp NOT NULL,
+	last_edited timestamp NOT NULL,
+	content text NOT NULL
+);
+
+CREATE TABLE public.folder 
+(
+	id serial PRIMARY KEY,
+	user_id integer NOT NULL,
+	parent_id integer,
+	title varchar(255) NOT NULL
+);
+
+ALTER TABLE public.user ADD CONSTRAINT user_fk0 FOREIGN KEY (last_edited_note) REFERENCES public.note(id);
+
+ALTER TABLE public.note ADD CONSTRAINT note_fk0 FOREIGN KEY (user_id) REFERENCES public.user(id);
+ALTER TABLE public.note ADD CONSTRAINT note_fk1 FOREIGN KEY (folder_id) REFERENCES public.folder(id);
+
+ALTER TABLE public.folder ADD CONSTRAINT folder_fk0 FOREIGN KEY (user_id) REFERENCES public.user(id);
+ALTER TABLE public.folder ADD CONSTRAINT folder_fk1 FOREIGN KEY (parent_id) REFERENCES public.folder(id);
---
+++ db/project01/run.sql
@@ -0,0 +1,2 @@
+\i ./db/project01/delete.sql
+\i ./db/project01/create.sql
