656298ef7286f5d7b5b913924475e7062576cc58
per18020 <per18020@byui.edu>
2020-02-08 12:10:32 -0700

    Added more notes

--- db/project01/insert.sql
+++ db/project01/insert.sql
@@ -21,9 +21,27 @@ INSERT INTO public.folder (user_id, title) VALUES (
 
 INSERT INTO public.note (user_id, folder_id, title, creation, last_edited, data) VALUES (
     (SELECT id FROM public.user WHERE email = 'thefloatingtree@gmail.com'),
-    1,
-    'Test Note',
+    (SELECT id FROM public.folder WHERE title = 'ziggy'),
+    'Test Note 1',
     NOW(),
     NOW(),
-    '[]'
+    '{"ops":[{"insert":"Test Note 1"},{"attributes":{"header":1},"insert":"\n"},{"insert":"\n"}]}'
+);
+
+INSERT INTO public.note (user_id, folder_id, title, creation, last_edited, data) VALUES (
+    (SELECT id FROM public.user WHERE email = 'thefloatingtree@gmail.com'),
+    (SELECT id FROM public.folder WHERE title = 'ziggy'),
+    'Test Note 2',
+    NOW(),
+    NOW(),
+    '{"ops":[{"insert":"Test Note 2"},{"attributes":{"header":1},"insert":"\n"},{"insert":"\n"}]}'
+);
+
+INSERT INTO public.note (user_id, folder_id, title, creation, last_edited, data) VALUES (
+    (SELECT id FROM public.user WHERE email = 'thefloatingtree@gmail.com'),
+    (SELECT id FROM public.folder WHERE title = 'twiggy'),
+    'Test Note 3',
+    NOW(),
+    NOW(),
+    '{"ops":[{"insert":"Test Note 3"},{"attributes":{"header":1},"insert":"\n"},{"insert":"\n"}]}'
 )
