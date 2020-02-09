b22a4a5275d43a5d44a600eb2e8bb94cbe262933
per18020 <per18020@byui.edu>
2020-02-09 10:48:40 -0700

    Fixed some project organization

--- web/index.php
+++ web/index.php
@@ -1,55 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Homepage</title>
+header("Location:/home/pages/home.php");
 
-  <link rel="stylesheet" href="/lib/bulma.min.css">
-  <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-
-  <link rel="stylesheet" href="/home/style/index.css">
-</head>
-
-<body>
-  <div class="container">
-    <div id="panel-collapse-button">
-      <span class="icon">
-        <i class="fas fa-compress"></i>
-      </span>
-    </div>
-  </div>
-  <section id="active-area" class="hero is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <div class="columns">
-          <div class="column is-one-third">
-            <nav id="panel" class="panel is-link">
-              <div class="panel-heading">
-                <span>Homepage</span>
-              </div>
-              <p class="panel-tabs">
-                <a id="panel-tabs-home">Home</a>
-                <a id="panel-tabs-assignments">Assignments</a>
-                <a id="panel-tabs-team">Team</a>
-              </p>
-              <div id="nav-panel">
-                <div class="panel-block">
-                  <progress class="progress is-small" max="100"></progress>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="threejs"></div>
-  </section>
-
-  <script type="module" src="/home/script/app.js"></script>
-  <script type="module" src="/home/script/index.js"></script>
-</body>
-
-</html>
+?>
