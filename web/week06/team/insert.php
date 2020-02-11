c8a3454c981b513a0281bfc41ef3871b8e624b99
per18020 <per18020@byui.edu>
2020-02-11 16:25:15 -0700

    incremental

---
+++ web/week06/team/insert.php
@@ -0,0 +1,32 @@
+<?php
+$book = $_POST['book'];
+$chapter = $_POST['chapter'];
+$verse = $_POST['verse'];
+$content = $_POST['content'];
+
+require("dbConnect.php");
+$db = get_db();
+
+try
+{
+	$query = 'INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':book', $book);
+	$statement->bindValue(':chapter', $chapter);
+    $statement->bindValue(':verse', $verse);
+    $statement->bindValue(':content', $content);
+	$statement->execute();
+	
+	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
+	// get id of last inserted row - save in $userId
+	$userId = $db->lastInsertId("scripture");
+}
+catch (Exception $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+header("Location: display.php/?scriptureId=$scripture");
+
+die(); 
+?>
--- web/week06/team/run.sql
+++ web/week06/team/run.sql
@@ -1,3 +1,34 @@
+DROP TABLE if EXISTS topic CASCADE;
+DROP TABLE if EXISTS scripture CASCADE;
+DROP TABLE if EXISTS linker CASCADE;
 CREATE TABLE topic (
-    
-)
+   topic_id    SERIAL      NOT NULL PRIMARY KEY,
+   topic_name  VARCHAR(50) NOT NULL
+);
+CREATE TABLE scripture (
+   scripture_id   SERIAL   NOT NULL PRIMARY KEY,
+   book           TEXT     NOT NULL,
+   chapter        INTEGER  NOT NULL,
+   verse          INTEGER  NOT NULL,
+   content        TEXT     NOT NULL
+);
+CREATE TABLE linker (
+   linker_id      SERIAL   NOT NULL PRIMARY KEY,
+   topic_id       INTEGER  NOT NULL REFERENCES topic(topic_id),
+   scripture_id   INTEGER  NOT NULL REFERENCES scripture(scripture_id)
+);
+INSERT INTO topic (
+   topic_name
+) VALUES (
+   'Faith'
+);
+INSERT INTO topic (
+   topic_name
+) VALUES (
+   'Sacrafice'
+);
+INSERT INTO topic (
+   topic_name
+) VALUES (
+   'Charity'
+);
