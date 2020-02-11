1dcbd506b1e2ada3e5a44d0f14ad56bf139b1ce7
per18020 <per18020@byui.edu>
2020-02-11 16:29:37 -0700

    incremental

--- web/week06/team/index.php
+++ web/week06/team/index.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <?php
-	// require("dbConnect.php");
-	// $db = get_db();
+	require("dbConnect.php");
+	$db = get_db();
 ?>
 <body>
    <div class="container" style="margin-top:50px;">
@@ -17,18 +17,18 @@
                <input type="number" class="form-control" placeholder="Verse" name="verse">
             </div>
             <div class="col">
-               <input type="textarea" class="form-control" placeholder="Content" name="content">
+               <textarea name="content" class="form-control">Content</textarea>
             </div>
             <div class="col">
                   <select id="inputFood" class="form-control" name="food">
                      <?php
-                        $statement = $db->prepare("SELECT * FROM w6_food");
+                        $statement = $db->prepare("SELECT * FROM topic");
                         $statement->execute();
                         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
                         {
-                           $id   = $row['id'];
-                           $food = $row['food'];
-                           echo "<option value='$id'>$food</option>";
+                           $id   = $row['topic_id'];
+                           $topic_name = $row['topic_name'];
+                           echo "<option value='$id'>$topic_name</option>";
                         }
                      ?>
                   </select>
