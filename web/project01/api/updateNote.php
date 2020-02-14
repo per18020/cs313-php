4a199946af5c7c1c7ee672189aa63545dbfbc2b8
Jacob A. Perry <thefloatingtree@byui.edu>
2020-02-14 11:48:42 -0700

    debugging

--- web/project01/api/updateNote.php
+++ web/project01/api/updateNote.php
@@ -21,11 +21,11 @@ try {
     exit;
 }
 
-$query = 'UPDATE public.note SET folder_id=:folder_id, title=:note_title, last_edited=Now(), data=:note_data WHERE user_id=:user_id AND id=:note_id';
+$query = 'UPDATE public.note SET folder_id=:folder_id, title=:note_title, last_edited=Now(), data="$note_data" WHERE user_id=:user_id AND id=:note_id';
 $statement = $db->prepare($query);
 $statement->bindValue(':folder_id', $folder_id);
 $statement->bindValue(':note_title', $note_title);
-$statement->bindValue(':note_data', $note_data);
+// $statement->bindValue(':note_data', $note_data);
 $statement->bindValue(':user_id', $user_id);
 $statement->bindValue(':note_id', $note_id);
 $statement->execute();
